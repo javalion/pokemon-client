21315ae0b7ab621b3a4e8957d6f07981218c27a9
Terry McKee <javalion@hotmail.com>
2025-02-05 15:27:07 -0600

    Print out name and rarity

--- main.py
+++ main.py
@@ -4,7 +4,8 @@ import requests
 def main() -> None:
     print("Starting Pokemon Client")
     response = requests.get("https://api.tcgdex.net/v2/en/cards/swsh3-136")
-    print(response.json())
+    print(response.json()["name"])
+    print(response.json()["rarity"])
     print("Stopping Pokemon Client")
 
 if __name__ == "__main__":
