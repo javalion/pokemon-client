fc76f0cba37b9c69f5a79c8e9ee7f4f553f53af7
Terry McKee <javalion@hotmail.com>
2025-02-05 06:55:26 -0600

    Establish REST call

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
+import requests
 
 
 def main() -> None:
     print("Starting Pokemon Client")
-
+    response = requests.get("https://api.tcgdex.net/v2/en/cards/swsh3-136")
+    print(response.json())
     print("Stopping Pokemon Client")
 
 if __name__ == "__main__":
